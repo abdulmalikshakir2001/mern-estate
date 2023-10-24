40d29bc7dd99963077cec644d8fed674d4f50fb5
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2023-10-24 14:01:23 +0500

    server and client setup with pages & header complete

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+        Sign up 
+      
+    </div>
+  )
+}
+
+export default SignUp
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div>
+        Sgin in 
+      
+    </div>
+  )
+}
+
+export default Signin
