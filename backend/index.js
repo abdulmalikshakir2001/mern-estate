8b5a66887eea0a4e8f699ce8eb783d45cbb65d9c
abdulmalishakir2001 <abdulmalikshakir2001@gmail.com>
2023-10-24 19:07:55 +0500

    connect to data base

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
 import express from "express";
+
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
 const app =  express();
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('mongo db  conneccted')
+}).catch((err)=>{
+    console.log(`ERROR:::::::::::::::::::::::::::::${err}`)
+})
+
+
+
+
+
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
 
